931b12b9d57cb38e6307172d2465754f2c7572aa
Andres Muñoz <andru2015.7@gmail.com>
2024-04-26 01:15:45 -0500

    Adding seed data with migrations

--- Connection/TaskContext.cs
+++ Connection/TaskContext.cs
@@ -1,3 +1,4 @@
+using System.IO.Compression;
 using Microsoft.EntityFrameworkCore;
 using ProjectEF.Models;
 
@@ -12,14 +13,29 @@ public class TaskContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) 
     {
+        List<Category> initCategories = new List<Category>
+        {
+            new Category() { CategoryId = Guid.Parse("c00bf160-cd2a-4563-a552-37d70e46f81e"), Name = "Pendientes", Description = "Actividades pendientes por realizar", Weight = 20 },
+            new Category() { CategoryId = Guid.Parse("c00bf160-cd2a-4563-a552-37d70e46f82f"), Name = "Personales", Description = "Actividades personales", Weight = 30 }
+        };
+
         modelBuilder.Entity<Category>(category => 
         {
             category.ToTable("Category");
             category.HasKey(p => p.CategoryId);
 
             category.Property(p => p.Name).IsRequired().HasMaxLength(150);
-            category.Property(p => p.Description);
+            category.Property(p => p.Description).IsRequired(false);
+            category.Property(p => p.Weight);
+
+            category.HasData(initCategories);
         });
+
+        List<Models.Task> initTasks = new List<Models.Task>
+        {
+            new Models.Task() { TaskId = Guid.Parse("c00bf160-cd2a-4563-a552-37d70e46f83c"), Title = "Lavar ropa", CategoryId = Guid.Parse("c00bf160-cd2a-4563-a552-37d70e46f81e"),  Description = "Lavar la ropa sucia", TaskPriority = Priority.Medium, CreationDate = DateTime.Now },
+            new Models.Task() { TaskId = Guid.Parse("c00bf160-cd2a-4563-a552-37d70e46f84d"), Title = "Motilarme", CategoryId = Guid.Parse("c00bf160-cd2a-4563-a552-37d70e46f82f"),  Description = "Ir a la peluquería", TaskPriority = Priority.Medium, CreationDate = DateTime.Now },
+        };
 
         modelBuilder.Entity<Models.Task>(task => 
         {
@@ -28,10 +44,12 @@ public class TaskContext : DbContext
 
             task.HasOne(p => p.Category).WithMany(p => p.Tasks).HasForeignKey(p => p.CategoryId);
             task.Property(p => p.Title).IsRequired().HasMaxLength(200);
-            task.Property(p => p.Description);
+            task.Property(p => p.Description).IsRequired(false);
             task.Property(p => p.TaskPriority);
             task.Property(p => p.CreationDate);
             task.Ignore(p => p.Summary);
+            
+            task.HasData(initTasks);
         });
     }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -12,6 +12,8 @@ public class Category
     public string Name { get; set; }
 
     public string Description { get; set; }
+
+    public int Weight { get; set; }
     
     public virtual ICollection<Task> Tasks { get; set; }
 }
